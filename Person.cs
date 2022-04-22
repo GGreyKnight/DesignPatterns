bee1b715888fa4a6b5b16a9a87cd4bb17c293486
GGreyKnight <damiankarwowski9@gmail.com>
2022-04-22 22:18:45 +0200

    Dodaj pliki projektów.

---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    internal class Person
+    {
+        string name;
+        string age;
+        public string Name { get { return name; } }
+        public string Age { get { return age; } }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatterns;
+
+Console.WriteLine("Hello, World!");
+
+Point point = new Point();
+
+point.x = 5;
+
+Console.WriteLine(point);
+
+Person person = new Person();
+
+class Point
+{
+    public int x;
+}
