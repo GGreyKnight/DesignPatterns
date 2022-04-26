8a40e7d8f98d752d98373ad2774cd244634a745c
GGreyKnight <damiankarwowski9@gmail.com>
2022-04-26 22:31:20 +0200

    Builder pattern

---
+++ Builder/InvoiceBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class InvoiceBuilder
+    {
+        private Invoice _invoice = new Invoice();
+
+        public Invoice Build()
+        {
+            return _invoice;
+        }
+
+        //zamiana wszystkich metod z typu void na typ invoice builder, aby uzyskać płynne wywołanie
+        public InvoiceBuilder SetVendor(string vendor)
+        {
+            _invoice.Vendor = vendor;
+
+            return this;
+        }
+
+        public InvoiceBuilder SetVendee(string vendee)
+        {
+            _invoice.Vendee = vendee;
+
+            return this;
+        }
+
+        public InvoiceBuilder SetNote(string note)
+        {
+            _invoice.Note = note;
+
+            return this;
+        }
+
+        public InvoiceBuilder SetLineItems(IEnumerable<string> liteItems)
+        {
+            _invoice.LineItems = liteItems;
+
+            return this;
+        }
+
+        public InvoiceBuilder SetInvoiceNumber(string number)
+        {
+            _invoice.Number = number;
+
+            return this;
+        }
+
+        public InvoiceBuilder SetDate(DateTime date)
+        {
+            _invoice.Date = date;
+
+            return this;
+        }
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,21 @@
+﻿using Builder;
+
+var builder = new InvoiceBuilder();
+
+/*builder.SetDate(new DateTime(2020, 1, 1));
+builder.SetInvoiceNumber("A1");
+builder.SetVendor("Google");
+builder.SetVendee("Vendee");
+builder.SetLineItems(new List<string>() { "Line item1", "Line item2" });
+
+Invoice invoice = builder.Build();*/
+
+Invoice invoice = builder.SetDate(new DateTime(2000, 1, 1))
+    .SetInvoiceNumber("A1")
+    .SetVendor("Google")
+    .SetVendee("Vendee")
+    .SetLineItems(new List<string>() { "Line item1", "Line item2" })
+    .Build();
+
+Console.WriteLine(invoice.Vendor);
+Console.WriteLine(invoice.Vendee);
