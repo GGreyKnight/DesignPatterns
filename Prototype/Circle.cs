7d6ac33265c8f590731c4ddfdab7cd447641e26c
GGreyKnight <damiankarwowski9@gmail.com>
2022-05-02 00:23:19 +0200

    Prototype pattern

---
+++ Prototype/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Circle : Shape
+    {
+        public int Radius { get; set; }
+        public override Shape Clone()
+        {
+            Circle cloneBase = (Circle)this.MemberwiseClone();
+            cloneBase.Border = new Border()
+            {
+                Size = Border.Size,
+                Color = Border.Color
+            };
+
+            return cloneBase;
+        }
+
+        public override void Render()
+        {
+            Console.WriteLine("Render circle");
+        }
+    }
+}
---
+++ Prototype/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Triangle : Shape
+    {
+        public override Shape Clone()
+        {
+            Triangle cloneBase = (Triangle)this.MemberwiseClone();
+            cloneBase.Border = new Border()
+            {
+                Size = Border.Size,
+                Color = Border.Color
+            };
+
+            return cloneBase;
+        }
+
+        public override void Render()
+        {
+            Console.WriteLine("Render triangle");
+        }
+    }
+}
