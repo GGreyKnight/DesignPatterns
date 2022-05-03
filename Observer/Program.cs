ddb4977443b0a0f8b5111f17bc7271a99138df1f
GGreyKnight <damiankarwowski9@gmail.com>
2022-05-03 19:16:18 +0200

    Observer pattern

---
+++ Observer/Program.cs
@@ -0,0 +1,15 @@
+﻿using Observer;
+
+var subscriber1 = new Subscriber("subscriber1");
+var subscriber2 = new Subscriber("subscriber2");
+
+var publisher = new Publisher();
+
+publisher.Subscribe(subscriber1);
+publisher.Subscribe(subscriber2);
+
+publisher.Notify("test message");
+
+publisher.Unsubscribe(subscriber1);
+
+publisher.Notify("test message2");
