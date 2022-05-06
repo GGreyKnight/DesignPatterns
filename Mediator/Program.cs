0c1381f86b1891968abdbd1d362abf449b0c27f2
GGreyKnight <damiankarwowski9@gmail.com>
2022-05-06 22:42:46 +0200

    Mediator pattern

---
+++ Mediator/Program.cs
@@ -0,0 +1,10 @@
+﻿using Mediator;
+
+Button submitButton = new Button();
+Checkbox clientType = new Checkbox();
+
+new RegisterClientView(clientType, submitButton);
+
+submitButton.Click();
+
+clientType.Select();
