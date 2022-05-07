d3ceade4285457f4cc240eee00bc8cbdb255228f
GGreyKnight <damiankarwowski9@gmail.com>
2022-05-07 23:00:37 +0200

    Chain of responsibility pattern

---
+++ ChainOfResponsibility/AutorizationHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    public class AutorizationHandler : BaseHandler
+    {
+        private Dictionary<int, int> entityOwners = new Dictionary<int, int>()
+        {
+            {100, 13},
+            {101, 14}
+        };
+
+        public AutorizationHandler(IHandler next) : base(next)
+        {
+
+        }
+
+        public override void Handle(RequestContext requestContext)
+        {
+            Console.WriteLine("AutorizationHandler");
+
+            if(requestContext.Request.UserRole == "Admin")
+            {
+                _next.Handle(requestContext);
+                return;
+            }
+
+            if(entityOwners.TryGetValue(requestContext.Request.EntityId, out int ownerId))
+            {
+                if(ownerId == requestContext.Request.UserId)
+                {
+                    _next.Handle(requestContext);
+                    return;
+                }
+            }
+
+            requestContext.Response.IsSuccessful = false;
+            requestContext.Response.Message = "User is not authorized";
+        }
+    }
+}
