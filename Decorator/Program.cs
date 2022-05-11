fcc30d011595747156cdc5a7f7f546c3e02c95e1
GGreyKnight <damiankarwowski9@gmail.com>
2022-05-11 21:43:50 +0200

    Decorator pattern

---
+++ Decorator/Program.cs
@@ -0,0 +1,13 @@
+﻿using Decorator;
+
+var pizzaBase = new MediumPizza();
+
+var mediumPizzaWithCheese = new CheesePizzaDecorator(pizzaBase);
+
+Console.WriteLine($"mediumPizzaWithCheese: {mediumPizzaWithCheese.CalculatePrice()}");
+
+var p2 = new SalamiPizzaDecorator(mediumPizzaWithCheese);
+Console.WriteLine($"medium with cheese and salami : {p2.CalculatePrice()}");
+
+var doubleSalamiCheesePizza = new SalamiPizzaDecorator(p2);
+Console.WriteLine(doubleSalamiCheesePizza.CalculatePrice());
